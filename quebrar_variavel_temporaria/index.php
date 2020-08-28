bd1ad6547e69a995f2a7560001b0d3f2447655bc
Márcio Miranda <ti.marcio.miranda@drummond.com.br>
2020-08-28 17:07:34 -0300

    add

--- quebrar_variavel_temporaria/index.php
+++ quebrar_variavel_temporaria/index.php
@@ -1,13 +1,23 @@
 <?php declare(strict_types=1);
 
+//Com refatoração
 function retangulo(int $altura, int $largura): void
 {
+    $perimetro = 2 * ($altura + $largura);
+    $area = $altura * $largura;
 
-    $temp = 2 * ($altura + $largura);
-    echo "Perímetro: $temp" . "<br />";
-
-    $temp = $altura * $largura;
-    echo "Área: $temp";
+    echo "Perímetro: $perimetro" . "<br />";
+    echo "Área: $area";
 }
 
 retangulo(3, 10);
+
+//Sem refatoração
+//function retangulo(int $altura, int $largura): void
+//{
+//    $temp = 2 * ($altura + $largura);
+//    echo "Perímetro: $temp" . "<br />";
+
+//    $temp = $altura * $largura;
+//    echo "Área: $temp";
+//}
--- remover_atribuicao_a_parametro/index.php
+++ remover_atribuicao_a_parametro/index.php
@@ -1,29 +1,68 @@
 <?php declare(strict_types=1);
 
-function recebeDesconto(int $descontoInicial, bool $ehPremium, int $quantidade, int $anosCliente): void
+//Com refatoração
+function recebeDesconto(int $descontoInicial, bool $ehPremium, int $quantidadeCompras, int $anosCliente): void
 {
+    //A variável $descontoInicial contém o valor inicial,
+    //já a variável $desconto contém a referência inicial
+    //e posteriormente o cálculo executado sobre ela
+    //Evitando que o parâmetro $descontoInicial seja sobrescrito
+    $desconto = $descontoInicial;
 
-    if ($descontoInicial > 200) {
-        return;
+    if ($desconto > 200) {
+        return; //Saindo da função sem retornar nenhum valor
     }
 
     if ($ehPremium === true) {
-        $descontoInicial = $descontoInicial * 1.1;
+        $desconto = $desconto * 1.1;
     }
 
-    if ($quantidade > 50) {
-        $descontoInicial = $descontoInicial * 1.2;
+    if ($quantidadeCompras > 50) {
+        $desconto = $desconto * 1.2;
     }
 
     if ($anosCliente > 3) {
-        $descontoInicial = $descontoInicial * 1.1;
+        $desconto = $desconto * 1.1;
     }
 
     echo <<<EOF
---------------------
-Desconto: $descontoInicial
---------------------
-EOF;
+        --------------------
+        Desconto Inicial: $descontoInicial
+        --------------------
+    EOF;
+    echo "<br />";
+    echo <<<EOF
+        --------------------
+        Desconto após as compras: $desconto
+        --------------------
+    EOF;
 }
 
 recebeDesconto(50, true, 100, 100);
+
+//Sem refatoração
+//function recebeDesconto(int $descontoInicial, bool $ehPremium, int $quantidadeCompras, int $anosCliente): void
+//{
+//    if ($descontoInicial > 200) {
+//        return;
+//    }
+//
+//    if ($ehPremium === true) {
+//        $descontoInicial = $descontoInicial * 1.1;
+//    }
+//
+//    if ($quantidadeCompras > 50) {
+//        $descontoInicial = $descontoInicial * 1.2;
+//    }
+//
+//    if ($anosCliente > 3) {
+//        $descontoInicial = $descontoInicial * 1.1;
+//    }
+//
+//    echo <<<EOF
+//--------------------
+//Desconto: $descontoInicial
+//--------------------
+//EOF;
+
+//recebeDesconto(50, true, 100, 100);
